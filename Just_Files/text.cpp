79319819fce35beb5b8ce5a49661478efada18a1
Hazem-BackEnd <dondeo329@gmail.com>
2025-03-24 15:46:42 +0200

    ToMakeSure

---
+++ Just_Files/text.cpp
@@ -0,0 +1,5 @@
+//
+// Created by zemonze on 3/11/25.
+//
+
+#include "text.h"
---
+++ Just_Files/text.h
@@ -0,0 +1,16 @@
+//
+// Created by zemonze on 3/11/25.
+//
+
+#ifndef TEXT_H
+#define TEXT_H
+
+
+
+class text {
+
+};
+
+
+
+#endif //TEXT_H
